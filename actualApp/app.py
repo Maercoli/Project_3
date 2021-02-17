9d4d2f7ec23c1b604633197139142dadf1a96e4a
monica <monicaalina58@gmail.com>
2021-02-17 01:22:41 -0500

    code cleaning

--- actualApp/app.py
+++ actualApp/app.py
@@ -25,11 +25,8 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgres://xbrqktwe:E_nywn4IJhz89kqxwG3M498kv8qnq4Z6@rajje.db.elephantsql.com:5432/xbrqktwe"
-# engine = create_engine('postgres://xbrqktwe:E_nywn4IJhz89kqxwG3M498kv8qnq4Z6@rajje.db.elephantsql.com:5432/xbrqktwe')
+
 engine=create_engine('postgres://gvrdqgbticobzd:62de2f0015e8651bd5c46847dad2dde40c5d646fe973a52346228fb03564862d@ec2-100-24-139-146.compute-1.amazonaws.com:5432/d3j1lp4tuvm6l1')
-# Remove tracking modifications
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -38,11 +35,9 @@ Base.prepare(engine, reflect=True)
 CovidCases_On=Base.classes.CovidCases_On
 covid_dataset=Base.classes.covid_dataset
 
-#session = Session(engine)
 
 db = SQLAlchemy(app)
 
-#Pet = create_classes(db)
 
 # create route that renders index.html template
 @app.route("/")
@@ -53,11 +48,7 @@ def home():
 def start_map():
     return render_template("map.html")
 
-# @app.route("/api/v1/data")
-# def get_data():
-#     result=pd.read_sql('select * from "CovidCases_On";', engine)
-#     result_dict=result.to_dict(orient='list')
-#     return jsonify(result_dict)
+
 
 @app.route("/api/v1/dataX")
 def get_dataX():
@@ -95,29 +86,15 @@ def get_covid():
 
 
 @app.route("/api/v2/covidTmp")
-# I will remove that one once it starts working (Emilia)
 
 def get_covid_tmp():
     
     session = Session(engine)
     
     covid_list_tmp = []
-#covid_dataset
-#session.query(Table.column, func.count(Table.column)).group_by(Table.column).all()
-##    //with engine.connect() as con:
-##   //    query = """SELECT "Reporting_PHU_City","month", "Reporting_PHU_Latitude", "Reporting_PHU_Longitude" FROM "covid_dataset" """
-##    //    result_tmp = con.execute(query)
-    ###covid_list_tmp = session.query(covid_dataset.Reporting_PHU_City, covid_dataset.month,covid_dataset.Reporting_PHU_Latitude, covid_dataset.Reporting_PHU_Longitude, func.count(covid_dataset.Row_ID)).group_by(covid_dataset.Reporting_PHU_City,covid_dataset.month, covid_dataset.Reporting_PHU_Latitude,covid_dataset.Reporting_PHU_Longitude).order_by(covid_dataset.Reporting_PHU_City, covid_dataset.month).all()
+
     covid_list_tmp = session.query(covid_dataset.Reporting_PHU_City, covid_dataset.Reporting_PHU_Latitude, covid_dataset.Reporting_PHU_Longitude, func.count(covid_dataset.Row_ID)).group_by(covid_dataset.Reporting_PHU_City, covid_dataset.Reporting_PHU_Latitude,covid_dataset.Reporting_PHU_Longitude).order_by(covid_dataset.Reporting_PHU_City).all()
 
-    
-#    for row in result_tmp:
-#        Reporting_PHU_City = row[0]
-#        month = row[1]
-#        Reporting_PHU_Latitude = row[2]
-#        Reporting_PHU_Longitude = row[3]
-#        #covid_cases_data = row[4]
-#        covid_list_tmp.append({"Reporting_PHU_City": Reporting_PHU_City, "month":month ,"Reporting_PHU_Latitude":Reporting_PHU_Latitude, "Reporting_PHU_Longitude":Reporting_PHU_Longitude})
     session.close()
     return jsonify(covid_list_tmp)
     
